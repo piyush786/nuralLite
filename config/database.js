101022913310c4a1ff0663cdf4eb64068d660e02
Piyush Kapoor <piyushkapoor786@gmail.com>
2024-04-21 22:57:47 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoClient = require("mongodb").MongoClient
-const url = "mongodb://nuraltech:nuraltech%40123@165.232.190.44:27017/authSource=nuraltechLite&authMechanism=SCRAM-SHA-1";
+const url = "mongodb://nuraltech:nuraltech%40123@165.232.190.44:27017/nuraltechLite";
 module.exports = {
   url,
   mongoClient
