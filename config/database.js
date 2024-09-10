3e1ee0e4a4ffa0dd079e3803c605ffbb22f7394e
arpitNT <arpit@nuraltech.com>
2024-09-10 18:14:21 +0530

    updating previous changes in database made on instructions of Piyush sir

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
-const mongoClient = require("mongodb").MongoClient
-const url = "mongodb+srv://nuralLite:1380695dlFaAn7ut@NuralLiteDB-83ac9dfd.mongo.ondigitalocean.com/admin?retryWrites=true&w=majority";
+const mongoClient = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://NuralLiteDB-83ac9dfd.mongo.ondigitalocean.com/?retryWrites=true&replicaSet=NuralLiteDB&readPreference=primary&srvServiceName=mongodb&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=NuralLiteDB&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true&3t.sslTlsVersion=TLS";
 module.exports = {
   url,
-  mongoClient
-}
+  mongoClient,
+};
+
